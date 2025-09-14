cffd532ccecbdc0ef152ea1aac72e129559e2cbb
aliramazani <aliramazani.sw@gmail.com>
2025-09-14 01:22:59 -0400

    Update: add logger for db init

--- backend/database/init_db.py
+++ backend/database/init_db.py
@@ -1,11 +1,15 @@
 from backend.database.db_config import ps_db
 from backend.models import Office, Building, Contact
+from backend.Logger import get_logger
+
+logger = get_logger()
 
 def init_db():
     ps_db.connect(reuse_if_open=True)
     ps_db.drop_tables([Office, Contact, Building], safe=True, cascade=True)
-    ps_db.create_tables([Building, Contact, Office])  # IMPORTANT: parents first
-    print(f"✅ Database created successfully in {ps_db.database}.")
+    logger.info(f"✅ Tables dropped successfully in {ps_db.database}.")
+    ps_db.create_tables([Building, Contact, Office])
+    logger.info(f"✅ Tables created successfully in {ps_db.database}.")
     ps_db.close()
 
 if __name__ == "__main__":
